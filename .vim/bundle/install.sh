92c9b8d90b8690c00b59657e296f2ce0ccc905c5
Rory Hunter <roryhunter2@gmail.com>
2022-01-23 16:03:41 +0000

    Update

--- .vim/bundle/install.sh
+++ .vim/bundle/install.sh
@@ -1,27 +1,39 @@
 #!/bin/bash
 
-set -ex
+set -e
 
-git clone https://github.com/Chiel92/vim-autoformat
-git clone https://github.com/alunny/pegjs-vim
-git clone https://github.com/chase/vim-ansible-yaml
-git clone https://github.com/dagwieers/asciidoc-vim
-git clone https://github.com/derekwyatt/vim-scala
-git clone https://github.com/editorconfig/editorconfig-vim
-git clone https://github.com/ekalinin/Dockerfile.vim
-git clone https://github.com/fatih/vim-go
-git clone https://github.com/hashivim/vim-terraform
-git clone https://github.com/kchmck/vim-coffee-script
-git clone https://github.com/leafgarland/typescript-vim
-git clone https://github.com/mxw/vim-jsx
-git clone https://github.com/nelstrom/vim-subrip
-git clone https://github.com/pangloss/vim-javascript
-git clone https://github.com/pearofducks/ansible-vim
-git clone https://github.com/reasonml-editor/vim-reason-plus
-git clone https://github.com/tpope/vim-commentary
-git clone https://github.com/tpope/vim-fugitive
-git clone https://github.com/tpope/vim-repeat
-git clone https://github.com/tpope/vim-surround
-git clone https://github.com/udalov/kotlin-vim
-git clone https://github.com/vim-airline/vim-airline
-git clone https://github.com/w0rp/ale
+function clone () {
+  repo="$1"
+
+  dir="$(basename "$1")"
+
+  if [[ -d "$dir" ]]; then
+    (cd "$dir" && git pull "$repo")
+  else
+    git clone "$repo"
+  fi
+}
+
+clone https://github.com/Chiel92/vim-autoformat
+clone https://github.com/alunny/pegjs-vim
+clone https://github.com/chase/vim-ansible-yaml
+clone https://github.com/dagwieers/asciidoc-vim
+clone https://github.com/derekwyatt/vim-scala
+clone https://github.com/editorconfig/editorconfig-vim
+clone https://github.com/ekalinin/Dockerfile.vim
+clone https://github.com/fatih/vim-go
+clone https://github.com/hashivim/vim-terraform
+clone https://github.com/kchmck/vim-coffee-script
+clone https://github.com/leafgarland/typescript-vim
+clone https://github.com/mxw/vim-jsx
+clone https://github.com/nelstrom/vim-subrip
+clone https://github.com/pangloss/vim-javascript
+clone https://github.com/pearofducks/ansible-vim
+clone https://github.com/reasonml-editor/vim-reason-plus
+clone https://github.com/tpope/vim-commentary
+clone https://github.com/tpope/vim-fugitive
+clone https://github.com/tpope/vim-repeat
+clone https://github.com/tpope/vim-surround
+clone https://github.com/udalov/kotlin-vim
+clone https://github.com/vim-airline/vim-airline
+clone https://github.com/w0rp/ale
